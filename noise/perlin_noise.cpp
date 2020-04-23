17f202e7c3976adb20a38ed0fd9bb861635f0e0b
libreliu <libreliu@foxmail.com>
2020-04-23 17:53:03 +0800

    Added preliminary CMake framework

---
+++ noise/perlin_noise.cpp
@@ -0,0 +1,15 @@
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
+
+/* Perlin Noise Generation 
+ * - References:
+ *   - https://blog.csdn.net/qq_34302921/article/details/80849139
+ *   - https://zh.wikipedia.org/wiki/Perlin%E5%99%AA%E5%A3%B0
+ *   - Perlin, K. (1985). An image synthesizer. 19(3), 287–296. 
+ *     https://doi.org/10.1145/325334.325247
+ *   - TBC..
+ */
+
+int main(void) {
+
+}
